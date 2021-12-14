c9c06fd9be053a0f580a85d3c02ff4f8de0cc8fa
raj19986 <rajushekhawat1998@gmail.com>
2021-12-15 03:01:59 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -23,17 +23,16 @@ class App extends Component {
           <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
               
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
+              
               <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                   
                   
                   {
+                
                     ( auth ) ? 
                       ( <a className="nav-item nav-link" href="/" onClick={this.logOut}>Log out</a>) : 
-                      ( <Link className="nav-item nav-link float-right" to="/login">Log in</Link> )
+                      ( '')  
                    }
                 </div>
               </div>
